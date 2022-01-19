ae5e3890f9f8ff916cd91121618ee90b6753cc14
Tejas <tejas14all@gmail.com>
2022-01-19 06:59:11 +0530

    pract

---
+++ src/task/StringPract.java
@@ -0,0 +1,23 @@
+package task;
+
+import java.util.Scanner;
+
+public class StringPract {
+
+	public static void main(String[] args) {
+
+		Scanner scr = new Scanner(System.in);
+
+		System.out.println("Enter two string ");
+		String s1 = scr.next();
+		String s2 = scr.next();
+
+		if (s1.equalsIgnoreCase(s2)) {
+			System.out.println("Strings are same");
+		} else {
+			System.out.println("Strings are not same");
+		}
+
+		
+	}
+}
