3051807a6833e37d73b03a5b9ec1a6b9d61d9181
Tejas <tejas14all@gmail.com>
2022-01-26 07:29:59 +0530

    finally block

--- src/exceptiond/RuntimeErrorDemo.java
+++ src/exceptiond/RuntimeErrorDemo.java
@@ -12,19 +12,26 @@ public class RuntimeErrorDemo {
 		int a, b, c;
 
 		try {
-
+			//resource 
+			//open 
 			System.out.println("Enter two numbers ");
 			a = scr.nextInt();
 			b = scr.nextInt();
 			c = a / b;// 20/0 -> throw new ArithmeticException();
 			System.out.println(c);
+			//close resource
 		} catch (ArithmeticException e) {
 			System.out.println("Please do not enter zero in second value");
 		} catch (InputMismatchException e) {
 			System.out.println("Please enter only whole numbers");
 		} catch (Exception e) {
 			System.out.println("Something went wrong please try after sometime...");
+		} finally {
+			//close resource
+			System.out.println("i am always execute....");
 		}
 
+		// finally block will execute all the time
+		//gc 
 	}
 }
